edac1d5b4afa4bcfe68d1fcc58f7521cb71bd978
Mathieu <mrousseau9@icloud.com>
2023-06-17 18:30:59 -0400

    requis 6 pass

--- partie1/processor.js
+++ partie1/processor.js
@@ -3,6 +3,9 @@ function processor(transmission){
         return -1;
     }
     let parts = transmission.split("::");
+    if(parts[1].indexOf("<") != 0 || parts[1].indexOf(">") != parts[1].length -1){
+        return -1;
+    }
     return {
         id: Number(parts[0]),
         rawData: parts[1]
